226a9352977e7a3c121456344284b6032513565e
spesenti <silvana.pesenti@utoronto.ca>
2020-10-05 10:42:33 -0400

    path

--- R/mortality.R
+++ R/mortality.R
@@ -2,7 +2,7 @@
 setwd("~/STA2535")
 
 # Import dataset: Death_Probability_Canada_2018
-mort_2018 <- read.csv("~/data/Mortality-Canada-2018.csv")
+mort_2018 <- read.csv("data/Mortality-Canada-2018.csv")
 # let's have a look at the dataset
 head(mort_2018)
 
@@ -19,7 +19,7 @@ plot(mort_2018[,"Age.group"], log(mort_2018[, "VALUE"]), pch = 16, col = "green"
 
 # How does this compare to the mortality in 2008?
 # Import dataset: Death_Probability_Canada_2018
-mort_2008 <- read.csv("Mortality-Canada-2008.csv")
+mort_2008 <- read.csv("data/Mortality-Canada-2008.csv")
 
 plot(mort_2018[,"Age.group"], log(mort_2018[, "VALUE"]), pch = 16, col = "green", cex = 0.5,
      xlab = "age", ylab = "mortality - log-scale", main = "Canadian Mortality in 2018")
@@ -27,7 +27,7 @@ points(mort_2008[,"Age.group"], log(mort_2008[, "VALUE"]), pch = 16, col = "blue
 legend("topleft", legend = c("2018", "2008"), pch = 16, col = c("green", "blue"))
 
 
-mort_1998 <- read.csv("Mortality-Canada-1998.csv")
+mort_1998 <- read.csv("data/Mortality-Canada-1998.csv")
 # plotting all three
 plot(mort_2018[,"Age.group"], log(mort_2018[, "VALUE"]), pch = 16, cex = 0.5, col = "green",
      xlab = "age", ylab = "mortality - log-scale", main = "Canadian Mortality in 2018")
